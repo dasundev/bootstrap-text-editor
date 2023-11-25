a4767a8d845b41927e4d94b933d8947a419b71af
Dasun Tharanga <hello@dasun.dev>
2023-11-25 15:48:18 +0530

    Add Text Underline

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ function bold() {
     document.execCommand('bold', true, null)
 }
 
+function underline() {
+    document.execCommand('underline', true, null)
+}
+
 function italic() {
     document.execCommand('italic', true, null)
 }
